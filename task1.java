093192af64133df3bae9ca8c8eafed3308393845
Rodion <leon172071@gmail.com>
2022-09-22 01:40:54 +0300

    задача

---
+++ task1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int hours,minutes,second;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите кол-во часов");
+        hours = in.nextInt();
+        System.out.println("Введите кол-во минут");
+        minutes = in.nextInt();
+        System.out.println("Введите кол-во секунд");
+        second =in.nextInt();
+        System.out.println("время прошедшее с начала суток" +" "+ ((hours*60*60)+(minutes*60)+second));
+
+        }
+
+
+
+    }
