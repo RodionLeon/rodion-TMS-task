451fe82f54d21f0a99c8ce8648d020321bd17e4b
Rodion <leon172071@gmail.com>
2022-09-22 01:42:23 +0300

    задача

---
+++ task5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number,first,second,third;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите трёхзначное число");
+        number =in.nextInt();
+        first = (number - number % 100)/100;
+        second = (number /10) %10;
+        third = number %10 ;
+        System.out.println(third*100+second*10+first);
+    }}
